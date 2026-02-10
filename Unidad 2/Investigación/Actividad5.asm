d71070a1e1dcf974a5c4c539cea1496684204dc7
nataliapiedrahitaescobar-cpu <nataliapiedrahitaescobar@gmail.com>
2026-02-10 17:28:29 -0500

    Actividad 5 punto 1 completado

---
+++ Unidad 2/Investigación/Actividad5.asm
@@ -0,0 +1,20 @@
+//Convierte estos programas a ensamblador
+
+int a = 10;
+int* p;
+p = &a;
+*p = 20
+
+@10
+D=A 
+@x
+M=D
+@x
+D=A 
+@p
+M=D
+@10
+A=M
+@10
+D=A 
+M=D
---
+++ Unidad 2/Investigación/Actividad5.md
@@ -0,0 +1,2 @@
+Simulación ejercicio 1 actividad 5
+![alt text](image-4.png)
