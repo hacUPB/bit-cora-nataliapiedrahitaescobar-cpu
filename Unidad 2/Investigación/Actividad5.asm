96958e8feec2b7464a11cb333bdfd9c46c5753ff
nataliapiedrahitaescobar-cpu <nataliapiedrahitaescobar@gmail.com>
2026-02-10 17:31:37 -0500

    Punto 1 actividad 5 explicado + Inicio punto 2

--- Unidad 2/Investigación/Actividad5.asm
+++ Unidad 2/Investigación/Actividad5.asm
@@ -5,16 +5,27 @@ int* p;
 p = &a;
 *p = 20
 
+//Guardar el valor de 10
 @10
 D=A 
 @x
 M=D
+//Crear el puntero
 @x
 D=A 
 @p
 M=D
+//Apuntar a donde apunta el puntero
 @10
 A=M
+//El valor de x cambia usando el puntero
 @10
 D=A 
 M=D
+
+int a = 10;
+int b = 5;
+int *p;
+p = &a;
+b = p*
+
